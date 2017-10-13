48ed0dfe35d8f9affe0a50a4bb78e68d8d57b099
Hasan Al-Jawaheri <hasan.aljawaheri@gmail.com>
2017-10-14 00:52:17 +0300

    Fixed run script

--- run_server.sh
+++ run_server.sh
@@ -1,1 +1,5 @@
-python3.6 middleware.py -p 7990 -b blueprint_villa.json -i eth0 eth1 &
+#!/bin/sh
+cd /home/pi/middleware
+
+echo "Running the middleware..."
+python3.6 middleware.py -p 7990 -b blueprint_villa.json -i eth0 eth1 > middleware_log &
