262928eaf8aeedd4ea1d04bafdbe0aec3acb474c
Hasan Al-Jawaheri <hasan.aljawaheri@gmail.com>
2017-10-14 01:37:04 +0300

    redirect stderr to log as well

--- run_server.sh
+++ run_server.sh
@@ -2,4 +2,4 @@
 cd /home/pi/middleware
 
 echo "Running the middleware..."
-/usr/local/bin/python3.6 middleware.py -p 7990 -b blueprint_villa.json -i eth0 eth1 > middleware_log &
+/usr/local/bin/python3.6 middleware.py -p 7990 -b blueprint_villa.json -i eth0 eth1 1> middleware_log 2>&1 &
