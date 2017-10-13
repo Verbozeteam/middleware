cb140c679807d1a108c00b22112229b94e51819d
Hasan Al-Jawaheri <hasan.aljawaheri@gmail.com>
2017-10-14 01:30:22 +0300

    Fixed path in startup script

--- run_server.sh
+++ run_server.sh
@@ -2,4 +2,4 @@
 cd /home/pi/middleware
 
 echo "Running the middleware..."
-python3.6 middleware.py -p 7990 -b blueprint_villa.json -i eth0 eth1 > middleware_log &
+/usr/local/bin/python3.6 middleware.py -p 7990 -b blueprint_villa.json -i eth0 eth1 > middleware_log &
