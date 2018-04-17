4132ea4e106e4a07e907a739598fc64686a06580
Hasan-Jawaheri <hasan.aljawaheri@gmail.com>
2018-04-17 18:45:34 +0300

    Added support for virtual digital temp. sensor in honeywell thermostat

--- things/honeywell_thermostat.py
+++ things/honeywell_thermostat.py
@@ -6,6 +6,8 @@ class HoneywellThermostatT7560(Thing):
     def __init__(self, blueprint, thermostat_json):
         super(HoneywellThermostatT7560, self).__init__(blueprint, thermostat_json)
         self.port_array = ["d41", "d45", "d49", "d51", "d47", "d43", "a14", "a15"]
+        if hasattr(self, "temperature_port"):
+            self.input_ports[self.temperature_port] = 5000 # read temperature every 5 seconds
         for p in self.port_array:
             self.output_ports[p] = 1
         self.id = thermostat_json.get("id", "honeywell-thermostat")
@@ -24,6 +26,12 @@ class HoneywellThermostatT7560(Thing):
 
     def set_set_point(self, set_pt):
         self.current_set_point = float(min(max(set_pt, 12.0), 30.0))
+
+    def set_hardware_state(self, port, value):
+        super(HoneywellThermostatT7560, self).set_hardware_state(port, value)
+        if hasattr(self, "temperature_port") and port == self.temperature_port:
+            self.current_temperature = float(value) / 4.0
+        return False
 
     def get_state(self):
         return {
