2bebba66a4eb821adc11b8d6112928ba613ba1da
Hasan-Jawaheri <hasan.aljawaheri@gmail.com>
2018-05-02 17:03:12 +0300

    Implemented welcome light input & output

--- things/hotel_controls.py
+++ things/hotel_controls.py
@@ -5,10 +5,18 @@ import json
 class HotelControls(Thing):
     def __init__(self, blueprint, hotel_json):
         super(HotelControls, self).__init__(blueprint, hotel_json)
+        if not hasattr(self, "nocard_power_timeout"):
+            self.nocard_power_timeout = 20
+        if not hasattr(self, "card_in_state"): # card state/value that means "card is in"
+            self.card_in_state = 1
+        if not hasattr(self, "on_state"):
+            self.on_state = 1
+        if not hasattr(self, "welcome_light_duration"):
+            self.welcome_light_duration = 30
         if hasattr(self, "hotel_card"):
             self.input_ports[self.hotel_card] = {
                 "read_interval": 0, # 0 means read on-change
-                "is_pullup": True,
+                "is_pullup": self.card_in_state == 0,
             }
         self.output_ports[self.power_port] = 1 # digital output
         self.output_ports[self.do_not_disturb_port] = 1 # digital output
@@ -17,19 +25,22 @@ class HotelControls(Thing):
             self.input_ports[self.room_check_button] = {"read_interval": 0, "is_pullup": True}
         if hasattr(self, "bell_port"):
             self.output_ports[self.bell_port] = 1 # digital output (bell will be disabled when DND is on)
+        if hasattr(self, "welcome_input_port") and hasattr(self, "welcome_output_port"):
+            self.input_ports[self.welcome_input_port] = {
+                "read_interval": 0, # 0 means on-change
+                "is_pullup": self.card_in_state == 0
+            }
+            self.output_ports[self.welcome_output_port] = 1 # digital output
         self.id = hotel_json.get("id", "hotel-controls-" + self.power_port)
         self.card_in = 1
         self.do_not_disturb = 0
         self.room_service = 0
+        self.welcome_light = 0
         self.power = 1
         self.card_out_start = -1
-        if not hasattr(self, "nocard_power_timeout"):
-            self.nocard_power_timeout = 20
-        if not hasattr(self, "card_in_state"): # card state/value that means "card is in"
-            self.card_in_state = 1
-        if not hasattr(self, "on_state"):
-            self.on_state = 1
         self.room_check_status = 1 - self.card_in_state
+        self.door_open = 0
+        self.welcome_light_start_time = 0
 
     # Should return the key in the blueprint that this Thing captures
     @staticmethod
@@ -46,6 +57,8 @@ class HotelControls(Thing):
             self.card_in = 1 if value == self.card_in_state else 0
         elif hasattr(self, "room_check_button") and port == self.room_check_button:
             self.room_check_status = value
+        elif hasattr(self, "welcome_input_port") and port == self.welcome_input_port:
+            self.door_open = 1 if value == self.card_in_state else 0
         return False
 
     def set_state(self, data, token_from="system"):
@@ -59,20 +72,30 @@ class HotelControls(Thing):
 
     def update(self, cur_time_s):
         if self.card_in == 0:
+            # logic for sleep
             if self.card_out_start == -1:
                 self.card_out_start = cur_time_s
             elif cur_time_s - self.card_out_start > self.nocard_power_timeout:
                 self.card_out_start = cur_time_s # prevents spam commands
                 self.power = 0 # turn off power
+            # logic for door & welcome light
+            if self.door_open == 1: # if door is open and no card, turn on welcome light
+                self.welcome_light = 1
+                self.welcome_light_start_time = cur_time_s
         else:
+            # logic for wake-up
             if self.power == 0: # just turned on, wake Things up
                 things = self.blueprint.get_things()
                 for thing in things:
                     thing.wake_up()
             self.card_out_start = -1
             self.power = 1 # turn on power
+            # logic for door & welcome light
+            if cur_time_s - self.welcome_light_start_time >= self.welcome_light_duration:
+                self.welcome_light = 0
 
         if self.power == 0: # force sleep everything, every update...
+            self.welcome_light = 0
             things = self.blueprint.get_things()
             for thing in things:
                 thing.sleep()
@@ -104,5 +127,9 @@ class HotelControls(Thing):
         # ACTIVATE bell relay if DND is on (on ACTIVE it should cut the bell circuit)
         if hasattr(self, "bell_port"):
             state[self.bell_port] = dnd
+
+        # if welcome light output is present, set it
+        if hasattr(self, "welcome_light_output"):
+            state[self.welcome_output_port] = self.welcome_light if self.on_state == 1 else 1 - self.welcome_light
 
         return state
